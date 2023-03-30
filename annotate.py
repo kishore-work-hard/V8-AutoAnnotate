5871f3753e5aa0ca069df6e8f1375d427375820e
kishore-work-hard <93419928+kishore-work-hard@users.noreply.github.com>
2023-03-30 12:55:16 +0530

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -28,6 +28,7 @@ def coco(bbox, cls, frame):
     print("coco- ",coco_format)  # Output: "1 0.585156 0.200391 0.425547 0.393229"
     return coco_format
 
+
 while True:
     flag = False
     t1 = time.time()
@@ -38,8 +39,8 @@ while True:
         x = model.predict(source=frame)
         # print(x)
         coco_list=[]
-        cv2.imwrite(annotate_path + str(i) + ".jpg", frame)
         for box in x[0].boxes:
+            cv2.imwrite(annotate_path + str(i) + ".jpg", frame)
             print("box-",box.numpy())
             bbox =  box.numpy()
             xywh = bbox.xywh[0]
@@ -49,10 +50,14 @@ while True:
 
             # Get image filename without extension
             coco_list.append(coco_format)
-        filename, ext = os.path.splitext("tp" + str(i) + ".jpg")
+            if coco_list!=[]:
+                flag=True
+        if flag:
+            filename, ext = os.path.splitext("tp" + str(i) + ".jpg")
 
-        with open(annotate_path + str(i) + ".txt", "w") as f:
-            # Write contents of text list to file line by line
-            for line in coco_list:
-                f.write(line + "\n")
-        i = i + 1
+            with open(annotate_path + str(i) + ".txt", "w") as f:
+                # Write contents of text list to file line by line
+                for line in coco_list:
+                    f.write(line + "\n")
+            i = i + 1
+
